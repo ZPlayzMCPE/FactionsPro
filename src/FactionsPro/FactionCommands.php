dc7a50f297dacb6c8e06ac5414430068a2ce537d
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-01-09 17:11:34 +0000

    Update FactionCommands.php

--- src/FactionsPro/FactionCommands.php
+++ src/FactionsPro/FactionCommands.php
@@ -532,7 +532,7 @@ class FactionCommands {
                                         $this->plugin->db->query("DELETE FROM plots WHERE faction='$faction_ours';");
                                         $this->plugin->db->query("DELETE FROM plots WHERE faction='$faction_victim';");
                                         $arm = (($this->plugin->prefs->get("PlotSize")) - 1) / 2;
-                                        $this->plugin->newPlot($faction_ours, $x + $arm, string $z + $arm, Level $x - $arm, string $z - $arm);
+                                        $this->plugin->newPlot($faction_ours, $x + $arm, $z + $arm, $x2 - $arm, $z2 - $arm);
                                         $sender->sendMessage($this->plugin->formatMessage("The land of $faction_victim has been claimed. It is now yours.", true));
                                         return true;
                                     }
